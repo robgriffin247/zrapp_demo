f9255eb82cd78e35e7979bb9a6d56489a194c13c
Rob Griffin <robgriffin247@hotmail.com>
2025-11-07 20:38:45 +0100

    added a demo for getting all details of race entrants

---
+++ demos/race_data_demo.py
@@ -0,0 +1,45 @@
+import httpx
+import os
+import json 
+
+
+def get_event(id: int):
+    header = {'Authorization':os.getenv("ZRAPP_API_KEY")}
+    url = f'https://zwift-ranking.herokuapp.com/public/results/{id}'
+
+    response = httpx.get(url, headers=header)
+    response.raise_for_status()
+
+    data = response.json()
+
+    return data
+
+
+def get_riders(ids: list[int]):
+    header = {'Authorization':os.getenv("ZRAPP_API_KEY")}
+    url = f'https://zwift-ranking.herokuapp.com/public/riders/'
+
+    response = httpx.post(url,
+                          headers=header, 
+                          json=ids, 
+                          timeout=30)
+    response.raise_for_status()
+
+    content = response.content
+    decoded = content.decode(encoding='utf-8')
+    data = json.loads(decoded)
+
+    return data
+
+
+if __name__=="__main__":
+    # Get the riders that took part in the event
+    riders = get_event(5144359)
+
+    # Get the ids oft hose riders into a list of integers
+    ids = [rider["riderId"] for rider in riders]
+
+    # Pass the list of integers as payload for POST request to riders endopint
+    detailed_riders = get_riders(ids)
+    
+    print(detailed_riders)
