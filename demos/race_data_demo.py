b7c0a2912d2cf97adc7bde3573de09003d04a916
Rob Griffin <robgriffin247@hotmail.com>
2025-11-07 20:51:35 +0100

    added zp results approach

--- demos/race_data_demo.py
+++ demos/race_data_demo.py
@@ -32,14 +32,29 @@ def get_riders(ids: list[int]):
     return data
 
 
+def get_zp_results(id:int):
+    header = {'Authorization':os.getenv("ZRAPP_API_KEY")}
+    url = f'https://zwift-ranking.herokuapp.com/public/zp/{id}/results'
+    response = httpx.get(url, headers=header)
+    response.raise_for_status()
+    data = response.json()
+    return data
+
+
 if __name__=="__main__":
-    # Get the riders that took part in the event
-    riders = get_event(5144359)
+    # # First approach:
+    # # Get the riders that took part in the event
+    # riders = get_event(5144359)
 
-    # Get the ids oft hose riders into a list of integers
-    ids = [rider["riderId"] for rider in riders]
+    # # Get the ids oft hose riders into a list of integers
+    # ids = [rider["riderId"] for rider in riders]
 
-    # Pass the list of integers as payload for POST request to riders endopint
-    detailed_riders = get_riders(ids)
+    # # Pass the list of integers as payload for POST request to riders endopint
+    # detailed_riders = get_riders(ids)
     
-    print(detailed_riders)
+    # print(detailed_riders)
+
+    # Alternative:
+    riders = get_zp_results(5144359)
+    for rider in riders:
+        print(rider.get("male"))
